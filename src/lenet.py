1cf6fe2b1935fd577ccde960a9e2e709fe6eadfe
Mahdi Sajedi <77678847+MahdiSajedei@users.noreply.github.com>
2021-02-07 08:02:34 +0330

    Update lenet.py

--- src/lenet.py
+++ src/lenet.py
@@ -13,7 +13,7 @@ def batch_norm_relu(inputs, is_training, data_format):
   # We set fused=True for a significant performance boost. See
   # https://www.tensorflow.org/performance/performance_guide#common_fused_ops
   inputs = tf.layers.batch_normalization(
-      inputs=inputs, axis=1 if data_format == 'channels_first' else -1,
+      inputs=inputs, axis=1 if data_format == 'channels_first' else 3,
       momentum=_BATCH_NORM_DECAY, epsilon=_BATCH_NORM_EPSILON, center=True,
       scale=True, training=is_training, fused=True)
   
